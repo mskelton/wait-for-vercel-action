624c11b939bd3b7922f427c0ff7d801bac624eaa
Mark Skelton <mdskelton99@gmail.com>
2020-05-23 16:51:12 -0500

    Get prod url

--- index.js
+++ index.js
@@ -5,22 +5,37 @@ const axios = require("axios")
 const sleep = (seconds) =>
   new Promise((resolve) => setTimeout(resolve, seconds * 1000))
 
+const headers = {
+  headers: {
+    Authorization: `Bearer ${process.env.VERCEL_TOKEN}`,
+  },
+}
+
+async function getProdUrl(sha) {
+  const prodUrl = core.getInput("prod-url", { required: true })
+  const url = `https://api.vercel.com/v11/now/deployments/get?url=${prodUrl}`
+  const { data } = await axios.get(url, headers)
+
+  if (data.meta.githubCommitSha === sha) {
+    throw new Error("Commit sha for prod url didn't match")
+  }
+
+  return data.url
+}
+
 async function getBranchUrl(sha) {
   const url = `https://api.vercel.com/v5/now/deployments?meta-githubCommitSha=${sha}`
-  const { data } = await axios.get(url, {
-    headers: {
-      Authorization: `Bearer ${process.env.VERCEL_TOKEN}`,
-    },
-  })
+  const { data } = await axios.get(url, headers)
 
   // If the deployment isn't in the response, this will throw an error and
   // cause a retry.
-  return `https://${data.deployments[0].url}`
+  return data.deployments[0].url
 }
 
 function getUrl(sha) {
-  // return branch === "master" ? getProdUrl() : getBranchUrl(sha)
-  return getBranchUrl(sha)
+  return "https://" + github.context.payload.ref === "refs/heads/master"
+    ? getProdUrl(sha)
+    : getBranchUrl(sha)
 }
 
 async function waitForDeployment() {
@@ -44,7 +59,6 @@ async function waitForDeployment() {
 
 ;(async () => {
   try {
-    console.log(github.context.payload)
     const url = await waitForDeployment()
 
     console.log("Url found!", url)
